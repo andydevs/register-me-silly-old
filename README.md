c449185bcef6b4a3fef0b97710a53361d31be1c0
andydevs <akanshul97@gmail.com>
2018-09-21 19:26:43 -0400

    Removed setup.py (we're not packaging this). Added description

---
+++ README.md
@@ -0,0 +1,5 @@
+# Drexel Shaft Protection
+
+Prevents your tiny student butthole from being brutally violated by drexel's
+aweful course registration system. Repeatedly checks webtms for enrollment
+info of courses
---
+++ drexel_shaft_protection/__init__.py
@@ -0,0 +1,10 @@
+"""
+Drexel Shaft Protection
+
+Prevents your tiny student butthole from being brutally violated by drexel's
+aweful course registration system. Repeatedly checks webtms for enrollment
+info of courses
+
+Author:  Anshul Kharbanda
+Created: 9 - 21 - 2018
+"""
