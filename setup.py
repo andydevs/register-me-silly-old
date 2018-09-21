a8b9e0d53f126e0ae66709faef48eaf43a5c2aa3
andydevs <akanshul97@gmail.com>
2018-09-21 19:22:11 -0400

    Created empty .gitignore, LICENSE, and README.md files. Created setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Drexel Shaft Protection
+
+Prevents your tiny student butthole from being brutally violated by drexel's
+aweful course registration system. Repeatedly checks webtms for enrollment
+info of courses
+
+Author:  Anshul Kharbanda
+Created: 9 - 21 - 2018
+"""
+from distutils.core import setup
+
+# Setup function
+setup(name='drexel-shaft-protection',
+      version='0.1',
+      description='Repeatedly checks webtms for enrollment info of courses',
+      author='Anshul Kharbanda',
+      author_email='akanshul97@gmail.com',
+      homepage='http://github.com/andydevs/drexel-shaft-protection',
+      packages=['drexel_shaft_protection'])
