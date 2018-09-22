c3634938283e636fb0416d67dd14803e2daa3ea7
andydevs <akanshul97@gmail.com>
2018-09-21 20:39:38 -0400

    Implemented trigger and has_enrollment_available

--- drexel_shaft_protection/__init__.py
+++ drexel_shaft_protection/__init__.py
@@ -8,3 +8,50 @@ info of courses
 Author:  Anshul Kharbanda
 Created: 9 - 21 - 2018
 """
+from requests import post, get
+from bs4 import BeautifulSoup
+
+def has_enrollment_available(webpage):
+    """
+    Returns true if the webpage has enrollment available
+
+    :param webpage: the webpage of the class to pull from
+    """
+    # Get soup
+    soup = BeautifulSoup(get(webpage).content, 'html.parser')
+
+    def is_enrollment_row(tag):
+        """
+        Returns true if the tag is the enrollment row
+
+        :param tag: the tag to check
+
+        :return: true if the tag is the enrollment row
+        """
+        is_tr = tag.name == 'tr'
+        cells = tag.find_all('td')
+        has_2_cells = len(cells) == 2
+        is_enrollment_cell = 'enroll' in cells[0].string.lower()
+        return is_tr and has_2_cells and is_enrollment_cell
+
+    # Find enrollment row
+    enrollment = soup.find(is_enrollment_row)
+    if enrollment:
+        estring = enrollment.find_all('td')[1].string.lower()
+        return 'closed' in estring
+    else:
+        return True
+
+def trigger(event, data, key):
+    """
+    Trigger event on remote server with key
+
+    :param event: event name to trigger
+    :param data: the data to pass into the trigger requests
+    :param key: the api key
+
+    :return: response
+    """
+    return post(
+        f'https://maker.ifttt.com/trigger/{event}/with/key/{key}',
+        data=data)
